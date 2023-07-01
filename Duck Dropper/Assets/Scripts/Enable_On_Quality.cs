42f7c9eff8b2566ac07ee159342fa0017d438a7c
zackmichaels5 <59713280+zackmichaels5@users.noreply.github.com>
2023-07-01 10:19:49 -0400

    Changed section culling and fixed lighting bug
    Changed which sections have culling enabled around buildings to have more aggressive culling. Added a light blocking invisible cube to cast a shadow for a specific awning. This issue only affected fair settings so an extra script was made to only enable this shadow caster for that setting.

---
+++ Duck Dropper/Assets/Scripts/Enable_On_Quality.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enable_On_Quality : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private int qualitySetting;
+    [SerializeField] private bool activeOnQuality = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(QualitySettings.GetQualityLevel() == qualitySetting)
+        {
+            meshRenderer.enabled = activeOnQuality;
+        }
+        else
+        {
+            meshRenderer.enabled = !activeOnQuality;
+        }
+    }
+
+}

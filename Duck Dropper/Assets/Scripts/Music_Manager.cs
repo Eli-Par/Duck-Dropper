7f2396d27e20a65862f0557cb80a9254f6edb09a
zackmichaels5 <59713280+zackmichaels5@users.noreply.github.com>
2023-06-30 21:08:43 -0400

    Added music
    Added a music manager to each scene. There will only ever be one music manager and the music will not stop between scenes.

---
+++ Duck Dropper/Assets/Scripts/Music_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Manager : MonoBehaviour
+{
+    public static Music_Manager Instance { get; private set; }
+
+    private void Awake()
+    {
+        //Singleton pattern so there is only one gameobject with this code
+        DontDestroyOnLoad(this);
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}

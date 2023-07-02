63ba5502dce3b9bd336656af2220221f35a5b2c6
zackmichaels5 <59713280+zackmichaels5@users.noreply.github.com>
2023-07-02 17:03:11 -0400

    Added/Fixed screenshot system
    Added a proper implementation of the screenshot system to allow pressing f2 to take a screenshot. A canvas was added that has text on it to briefly show the file name so the user knows a screenshot was taken. The reason that this feature is technically fixed was because it is built using code that was added in the last commit to the menu to make the splash screen.

--- Duck Dropper/Assets/Scripts/Screnshot.cs
+++ Duck Dropper/Assets/Scripts/Screnshot.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
 
 public class Screnshot : MonoBehaviour
 {
     [SerializeField] private int superSize = 1;
+    [SerializeField] private TextMeshProUGUI text = default;
+    [SerializeField] private float showTime = 1f;
+
+    private string lastName = "";
+    private float timer = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +25,22 @@ public class Screnshot : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.F2))
         {
             string name = "Duck Dropper " + DateTime.Now.ToString("yyyy/MM/dd HH-mm-ss") + ".png";
+            lastName = name;
             ScreenCapture.CaptureScreenshot(name, superSize);
             Debug.Log("Screenshot " + name + " taken");
+            timer = 0;
         }
+
+        if(timer < showTime)
+        {
+            text.text = lastName;
+            text.gameObject.SetActive(true);
+        }
+        else
+        {
+            text.gameObject.SetActive(false);
+        }
+
+        timer += Time.unscaledDeltaTime;
     }
 }
